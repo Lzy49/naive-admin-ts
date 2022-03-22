b74e62ecfedf054a3a19c8f10dccae41546dd686
lzy49@qq.com <lzy49@qq.com>
2022-03-22 15:55:35 +0800

    test: 测试 unplugin-auto-import 和 unplugin-vue-components

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue';
-// 1
+// import HelloWorld from './components/HelloWorld.vue';
+const v = ref<number>(123);
+console.log(v);
 </script>
 
 <template>
